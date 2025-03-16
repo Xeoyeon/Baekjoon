97022c55a511b5c8e949c54affdb44862ba6ffc4
Xeoyeon <127655800+Xeoyeon@users.noreply.github.com>
2025-03-16 20:34:20 +0900

    [Bronze V] Title: X보다 작은 수, Time: 44 ms, Memory: 33564 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
@@ -1,8 +1,6 @@
 import sys
 a,b = map(int, sys.stdin.readline().strip().split())
 A = map(int, sys.stdin.readline().strip().split())
-arr=""
 for i in A:
     if i<b:
-        arr=arr+str(i)+" "
-print(arr)
+        print(i,end=" ")
