a45c1708fc50c34ebd4df6c2f4f277aa171eea84
Xeoyeon <127655800+Xeoyeon@users.noreply.github.com>
2025-03-30 18:23:43 +0900

    [Bronze III] Title: 대지, Time: 80 ms, Memory: 35916 KB -BaekjoonHub

---
+++ 백준/Bronze/9063. 대지/대지.py
@@ -0,0 +1,10 @@
+import sys
+N =int(sys.stdin.readline().rstrip())
+x=[]
+y=[]
+for i in range(N):
+    a, b = map(int, sys.stdin.readline().split())
+    x.append(a)
+    y.append(b)
+
+print((max(x)-min(x))*(max(y)-min(y)))
